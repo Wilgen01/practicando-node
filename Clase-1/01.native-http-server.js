cb8ec851031c1aa5f5f48975a82e5bf20667466f
Wilgen Garcia <85307285+Wilgen01@users.noreply.github.com>
2023-07-31 16:29:24 -0500

    add proccess env

--- Clase-1/01.native-http-server.js
+++ Clase-1/01.native-http-server.js
@@ -1,16 +1,15 @@
 const http = require("node:http");
 const { findPort } = require("./02.find-port.js");
 
-
+const PORT =  process.env.PORT ?? 3000;
 const server = http.createServer((req, res) => {
   console.log("request");
   res.end("Respuesta");
 });
 
-findPort(3000).then(port =>{
+findPort(PORT).then(port =>{
   server.listen(port, () => {
     console.log(`server live on port ${port}`);
   });
-
 })
 
