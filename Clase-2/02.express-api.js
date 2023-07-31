a8fd35319e5705acf5aba72f2fa886af141ecdf4
Wilgen Garcia <85307285+Wilgen01@users.noreply.github.com>
2023-07-31 18:15:12 -0500

    expres api

--- Clase-2/02.express-api.js
+++ Clase-2/02.express-api.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send({data : 'data'})
+})
+
+app.post('/as', (req, res) => {
+    const { body } = req
+
+    body.timestamp = '123456'
+    res.send(body)
 })
 
 app.listen(3000, () => {
