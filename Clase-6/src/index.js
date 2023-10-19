39e947a5878daaa540ebf69f7a6ac810abeaab59
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-10-19 11:44:12 -0500

    add socket

--- Clase-6/src/index.js
+++ Clase-6/src/index.js
@@ -1,13 +1,21 @@
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 const PORT = process.env.PORT || 3000
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on('connection', () => {
+    console.log("user connected by socket");
+})
 
 app.get('/', (req, res) => {
     res.sendFile(`${process.cwd()}/src/client/index.html`)
 })
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`running`);
 })
