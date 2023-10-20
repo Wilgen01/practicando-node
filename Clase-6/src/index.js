87c1a4dcc7dc616c3e91827586177ad39f5e4721
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-10-20 16:14:01 -0500

    realtime chat

--- Clase-6/src/index.js
+++ Clase-6/src/index.js
@@ -2,20 +2,30 @@ import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-io.on('connection', () => {
-    console.log("user connected by socket");
-})
+io.on("connection", (socket) => {
+  console.log("user connected by socket");
 
-app.get('/', (req, res) => {
-    res.sendFile(`${process.cwd()}/src/client/index.html`)
-})
+  io.emit("connection", socket.handshake.auth.username ?? "anónimo")
 
+  socket.on('disconnect', () => {
+    io.emit("disconnection", socket.handshake.auth.username ?? "anónimo")
+  })
+
+  socket.on("message", (message) => {
+    const username = socket.handshake.auth.username ?? "anónimo"
+    io.emit("message", message, username);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(`${process.cwd()}/src/client/index.html`);
+});
 
 server.listen(PORT, () => {
-    console.log(`running`);
-})
+  console.log(`running`);
+});
